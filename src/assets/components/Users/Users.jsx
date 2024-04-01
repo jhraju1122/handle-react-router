411b5c145ff3f09ec630c056439434cb63637746
jhraju <jhraju890@gmail.com>
2024-04-01 16:40:17 +0600

    added use loader data

--- src/assets/components/Users/Users.jsx
+++ src/assets/components/Users/Users.jsx
@@ -1,9 +1,16 @@
- 
+//  state--> data
+// state--> loader
+// useEffect
+// fetch ---> state set-->set state 
+
+import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
+    
+const users = useLoaderData();
     return (
         <div>
-            <h2>Our Users</h2>
+            <h2>Our Users: {users.length}</h2>
         </div>
     );
 };
